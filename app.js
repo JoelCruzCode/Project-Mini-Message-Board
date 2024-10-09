00ec526905c53a0d49b35a7a678b3920b34a8939
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-10-08 19:34:44 -0700

    feat:  add navbar

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const messagesRouter = require("./routes/messagesRouter");
 const path = require("node:path");
 const messages = require("./data/messages");
+const links = require("./data/links");
 
 const app = express();
 
@@ -10,7 +11,11 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Mini Message Board", messages: messages });
+  res.render("index", {
+    title: "Mini Message Board",
+    messages: messages,
+    links: links,
+  });
 });
 app.use("/messages", messagesRouter);
 
---
+++ data/links.js
@@ -0,0 +1,7 @@
+const links = [
+  { name: "Message Board", url: "/" },
+  { name: "Inbox", url: "/messages" },
+  { name: "Create Message", url: "messages/new" },
+];
+
+module.exports = links;
